940451bb67a23b775b8a2f02717bb9c8476490ac
Yahel Nachum <ynachum@wpi.edu>
2015-09-15 16:47:50 -0400

    Hello World with time keeping example

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,15 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		long before = System.currentTimeMillis();
 		System.out.println("Hellow World");
+		try {
+		    Thread.sleep(100000);                 //1000 milliseconds is one second.
+		} catch(InterruptedException ex) {
+		    Thread.currentThread().interrupt();
+		}
+		long after = System.currentTimeMillis();
+		System.out.println("Time to print \"Hellow World\": " + (after - before));
 	}
 
 }
